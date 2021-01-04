ec5da3c5026ee54505b37489b1cd31110dd9b128
MambusLunde <64388043+MambusLunde@users.noreply.github.com>
2021-01-03 17:01:51 -0800

    Update navigation.py

--- src/navigate/navigation.py
+++ src/navigate/navigation.py
@@ -6,6 +6,13 @@ def amazonSearch(search_term):
     
     amazon = "https://www.amazon.com"
 
+    
+    options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox") #This make Chromium reachable
+    options.add_argument("--no-default-browser-check") #Overrides default choices
+    options.add_argument("--no-first-run")
+    options.add_argument("--disable-default-apps") 
+
     driver = webdriver.Chrome()
     
     driver.get(amazon)
