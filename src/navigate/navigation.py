53c42f190600124efbac2391162b2ddea3fdb7e1
MambusLunde <64388043+MambusLunde@users.noreply.github.com>
2021-01-03 17:25:46 -0800

    Update navigation.py

--- src/navigate/navigation.py
+++ src/navigate/navigation.py
@@ -6,7 +6,12 @@ def amazonSearch(search_term):
     
     amazon = "https://www.amazon.com"
 
-    driver = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     
     driver.get(amazon)
     
